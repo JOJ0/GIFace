5fb77f3a0d86c394a0d7e0716005acf1e42144e5
J0J0 Todos <jojo@peek-a-boo.at>
2024-02-19 13:13:10 +0100

    Use first pic as face to be recognized further on

--- giface/commands/auto.py
+++ giface/commands/auto.py
@@ -21,37 +21,41 @@ from giface.config import valid_conf
 # @click.option('--start', '-s', type=int, default=1, show_default=True,
 #               help="Start naming pics and gallery files with this number.")
 def auto(source_images):
-    """ Generate a GIF from a bunch of pictures
+    """Generates animated GIF from a bunch of pictures
+
+    while using the first found face to recognize in subsequent pics.
 
     Takes a list of pictures as argument. Use shell globbing to pass a whole
     folder of pics (use /path/*)
     """
-    # images = [Image.open(fn) for fn in file_names]
-    size = (666,666)
+    size = (333, 333)
     new_width = 333
     new_height = 333
-
+    # Filter out non-valid files
+    image_paths = [
+            image for image in source_images
+            if (str(Path(image).suffix).lower()
+                in ['.jpg', '.jpeg', '.png'])
+    ]
+    # Use first picture as example for face to recognize
+    recognize_face = face_recognition.load_image_file(image_paths[0])
+    the_face_encoding = face_recognition.face_encodings(recognize_face)[0]
+    # Process rest of pictures
     images = []
-    for image in source_images:
-        # Filter out non-valid files
-        if (
-            Path(image).is_dir()
-            or str(Path(image).suffix).lower() not in ['.jpg', '.jpeg', '.png']
-        ):
-            continue
-
+    for image in image_paths:
         image_of_people = face_recognition.load_image_file(image)
         unknown_face_locations = face_recognition.face_locations(image_of_people)
-        for face_location in unknown_face_locations:
-            top, right, bottom, left = face_location
-            face_image = image_of_people[top:bottom, left:right]
-            im = Image.fromarray(face_image)
+        for face_location in unknown_face_locations:  # Loop found faces
+            top, right, bottom, left = face_location  # Save coordinates
+            found_face = image_of_people[top:bottom, left:right]
+            # Check if it's the face we are looking for
+            compared = face_recognition.compare_faces([the_face_encoding], found_face)
+            if compared[0] is True:
+                im = Image.fromarray(found_face)
+                im.thumbnail(size)  # Streamline size,
+                images.append(im)  # and finally save to images list
 
-        # Prepare image for GIF and add to list
-        im.thumbnail(size)  # Streamline size
-        images.append(im)
-
-
+    # Save gif
     images[1].save('final.gif', save_all=True, append_images=images[1:],
-            duration=333, loop=1)
+            duration=300, loop=1)
 
